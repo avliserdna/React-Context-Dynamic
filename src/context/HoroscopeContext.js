8db2e3a2a5b5cb3e1f1213ab4c4730925f8a033f
avliserdna <35595989+avliserdna@users.noreply.github.com>
2022-10-04 20:38:59 -0700

    Finished Practice 2

--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,3 +1,13 @@
-import { createContext } from 'react';
-
+import { createContext, useState } from "react";
+import { default as horoscopesObj } from "../data/horoscopes.js";
 export const HoroscopeContext = createContext();
+
+export const HoroscopeProvider = (props) => {
+  const [currentSign, setCurrentSign] = useState("Pisces");
+  const sign = horoscopesObj[currentSign];
+  return (
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  );
+};
